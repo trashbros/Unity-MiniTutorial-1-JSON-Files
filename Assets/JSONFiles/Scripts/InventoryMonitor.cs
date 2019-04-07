c07c59defc50361c801871f577474d27154bae02
Reakain <reakain@users.noreply.github.com>
2019-04-07 10:16:58 -0400

    Shrunk test for laziness sake, added a non functional link in the readme

--- Assets/JSONFiles/Scripts/InventoryMonitor.cs
+++ Assets/JSONFiles/Scripts/InventoryMonitor.cs
@@ -8,55 +8,21 @@ namespace UnityTutorials
     {     
         [Header("Usables")]
         public string[] usableItemNames;
-      
-        [Header("Collectables")]
-        public string[] collectableItemNames;
-      
-        [Header("Macguffins")]
-        public string[] keyItemNames;
-        
+             
         [Header("Item References")]
         public TextAsset usableItems;
-        public TextAsset collectableItems;
-        public TextAsset keyItems;
 
         private ItemDatabase usables;
-        private ItemDatabase collectables;
-        private ItemDatabase keys;
 
         void Awake()
         {
-            usables = ItemDatabase.CreateFromJSON(usableItems.text);
-            usableItemNames = new string[usables.items.Length];
-            collectables = ItemDatabase.CreateFromJSON(collectableItems.text);
-            collectableItemNames = new string[collectables.items.Length];
-            keys = ItemDatabase.CreateFromJSON(keyItems.text);
-            keyItemNames = new string[keys.items.Length];
-        }
-      
-        // Use this for initialization
-        void Start()
-        {
+            usables = ItemDatabase.CreateFromJSON(usableItems.text);   // Read in our Json file
+            usableItemNames = new string[usables.items.Length];        // Instantiate our names array
+            // Iterates through our items and populates the names array with the name of each item
             for (int i = 0; i < usables.items.Length; i++)
             {
                 usableItemNames[i] = usables.items[i].name;
             }
-
-            for (int i = 0; i < collectables.items.Length; i++)
-            {
-                collectableItemNames[i] = collectables.items[i].name;
-            }
-
-            for (int i = 0; i < keys.items.Length; i++)
-            {
-                keyItemNames[i] = keys.items[i].name;
-            }
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unity-MiniTutorial-1-JSON-Files
 Mini example of reading and handling JSON files in Unity
+
+See the associated walkthrough Here[Link].
